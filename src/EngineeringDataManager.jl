bcb17c7afabf1727422064e8a2c7e55491bbb7b5
Roman Mironov <manarom@yandex.ru>
2025-09-04 22:27:41 +0300

    search_depth_is_needed...

--- src/EngineeringDataManager.jl
+++ src/EngineeringDataManager.jl
@@ -1,5 +1,5 @@
 module EngineeringDataManager
-using XML,Sockets,JSON3,StructTypes,Observables
+using XML,Sockets,JSON3,StructTypes,Observables,OrderedCollections
 #include("EngineeringDataTCPserver.jl")
 export find_tag
 abstract type AbstractTagMatcher end
@@ -29,7 +29,8 @@ const XML_DOC =  begin
                         end 
                         out
                 end
-const ParamIDs = Ref(Dict{String,String}())
+const StringFloat = Union{String,Float64}
+const ParamIDs = Ref(OrderedDict{String,StringFloat}())
 is_xml_ok() = @assert isassigned(XML_DOC) "No document file assigned, use `read_engineering_data` function to assign the file"
 function read_engineering_data(;file_fullname::String = ENG_DATA_FILE[])
     try    
@@ -50,7 +51,7 @@ function material_nodes(;material_name = "")
         name_node = find_tag(mi,"Name")
         !isempty(name_node) || continue
         @assert length(name_node) == 1 "Names node vector has multiple elements ???"
-        !isempty(name_node[].children) || continue
+        has_single_child(name_node[]) || continue
         nd_cur = name_node[].children[]
         if !isnothing(nd_cur.value) && isa(nd_cur.value,String)
             push!(names,nd_cur.value)
@@ -60,13 +61,26 @@ function material_nodes(;material_name = "")
 end
 function fill_ids()
     meta_data_node = find_tag("Metadata")
+    D = ParamIDs[]
+    empty!(D)
     !isempty(meta_data_node) || return false
     param_details = find_tag(meta_data_node[],"ParameterDetails")
     for p_i in param_details
         !isnothing(p_i.attributes) || continue
         haskey(p_i.attributes,"id") || continue
-        current_key = p_i
+        current_key = p_i.attributes["id"]
+        name_node = find_tag(p_i,"Name")
+        for nm in name_node
+            has_single_child(nm) || continue
+            if nodetype(nm.children[])==XML.Text
+                D[current_key] = nm.children[].value
+            end
+        end 
+        #@show current_key
     end
+end
+function has_single_child(node)
+    return !isnothing(node.children) && length(node.children) == 1
 end
 function find_tag(tag::String="EngineeringData") 
     is_xml_ok()
--- src/debug_script.jl
+++ src/debug_script.jl
@@ -4,3 +4,4 @@ includet(raw"EngineeringDataManager.jl")
 data  = XML.read(EngineeringDataManager.ENG_DATA_FILE[],Node)
 
 EngineeringDataManager.read_engineering_data()
+param_details = EngineeringDataManager.find_tag(EngineeringDataManager.find_tag("Metadata")[],"ParameterDetails")
